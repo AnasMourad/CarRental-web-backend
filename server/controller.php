27076cdc3402f6d960e159a1dd75e4874d456f03
Anas Mourad <anasmouradb2a@gmail.com>
2016-07-28 13:51:03 -0500

    refactoring #3
    moved rental car to controller.

--- server/controller.php
+++ server/controller.php
@@ -114,6 +114,23 @@ function car_search($connection){
 
 
 }
+function process_rent_car($connection){
+
+    $current_user =  $_SESSION["user_name"];
+    //CATCHING SELECTED CAR ID USING HIDDEN INPUT//CAN BE MODIFIED LATER!
+    $carID = $_POST["hidden-input"];
+
+    //ADDING CAR RENTAL RECORD TO RENTAL TABLE
+    $query = "INSERT INTO rental (rentDate, status, customerID, carID)
+    VALUES (CURDATE(), 1, '$current_user',$carID)";
+
+
+    $res = mysqli_query($connection, $query);
+    //UPDATING CURRENT CAR TO RENTED
+    $query = "UPDATE car SET car.status=2 WHERE car.id = $carID";
+    $res = mysqli_query($connection, $query);
+
+}
 
 //request_type: rented_cars
 $request_type = $_POST["request_type"];
@@ -173,7 +190,9 @@ switch($request_type){
     case "car_search":
         car_search($connection);
         break;
-
+    case "rent_car":
+        process_rent_car($connection);
+        break;
 }
 
 //request_type=show_returned_cars
